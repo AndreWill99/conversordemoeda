26bfd5000c334e9efc7fa78f0a2f601004146d85
ANDRE WILLIAM <89886682+AndreWill99@users.noreply.github.com>
2022-05-09 23:39:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+async function convert(){
+var quantia = document.getElementById("quantia"). value
+var min = document.querySelector('input[name="in"]:checked').value
+var mout = document.querySelector('input[name="out"]:checked').value
+
+var url = "https://api.exchangerate.host/convert?from="+min+"&to="+mout
+  
+var response = await  fetch(url)
+var data  = await response.json()
+
+var  valorConvertido = data.info.rate * quantia
+
+document.getElementById("res").innerHTML = valorConvertido.toFixed(2)
+}
